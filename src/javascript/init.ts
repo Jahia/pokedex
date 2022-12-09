d9d2acffadbd02710cd752a56c891d4283d2054d
Florent BOURASSE <fbourasse@jahia.com>
2022-12-09 10:02:26 +0100

    Some modifications to make it work with TypeScript
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

---
+++ src/javascript/init.ts
@@ -0,0 +1,10 @@
+import {registry} from '@jahia/ui-extender';
+import register from './register';
+
+export default function () {
+    registry.add('callback', 'pokedex-init', {
+       targets: ['jahiaApp-init:50'],
+        callback: register
+    });
+}
+console.debug('%c Pokedex is activated', 'color: #3c8cba');
