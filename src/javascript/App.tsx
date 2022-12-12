1a9fbd5bf2683f77ef775d5434ea039094f2e0dd
Florent BOURASSE <fbourasse@jahia.com>
2022-12-12 09:49:54 +0100

    Use of useEffet to initialize the const pokemons
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/App.tsx
+++ src/javascript/App.tsx
@@ -1,10 +1,14 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent, useState, useEffect} from 'react';
 import POKEMONS from "./models/mock-pokemon";
 import Pokemon from "./models/pokemon";
 
 
 const App: FunctionComponent = () => {
-    const [pokemons] = useState<Pokemon[]>(POKEMONS);
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(()=> {
+        setPokemons(POKEMONS);
+    }, []);
 
     return (
         <div>
