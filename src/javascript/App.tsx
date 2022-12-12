8caefa69855d131446fae668d310819eedf972c9
Florent BOURASSE <fbourasse@jahia.com>
2022-12-12 11:21:43 +0100

    Display of Pokemons
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/App.tsx
+++ src/javascript/App.tsx
@@ -5,17 +5,30 @@ import Pokemon from "./models/pokemon";
 
 const App: FunctionComponent = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-    useEffect(()=> {
+    useEffect(() => {
         setPokemons(POKEMONS);
     }, []);
 
-    return (
-        <div>
-            <h1>Pokédex</h1>
-            <p>There are {pokemons.length} pokemons.</p>
-        </div>
-    )
+    return (<div
+                 style={{overflowY: 'scroll'}}>
+            <h1 className="center">Pokedex</h1>
+            <div className="container">
+                <div className="row">
+                    {pokemons.map(pokemon => (<div className="col s6 m4" key={pokemon.id}>
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img src={pokemon.picture} alt={pokemon.name}/>
+                                </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <p>{pokemon.name}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>))}
+                </div>
+            </div>
+        </div>)
 }
 
 export default App;
