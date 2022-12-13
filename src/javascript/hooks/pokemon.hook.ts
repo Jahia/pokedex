ba1fa14239ea0505da99f7d5f442c0a08e87749a
Florent BOURASSE <fbourasse@jahia.com>
2022-12-13 10:37:57 +0100

    Usage of a personalized hook
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

---
+++ src/javascript/hooks/pokemon.hook.ts
@@ -0,0 +1,14 @@
+import {useEffect, useState} from "react";
+import Pokemon from "../models/pokemon";
+import POKEMONS from "../models/mock-pokemon";
+
+const usePokemons = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        setPokemons(POKEMONS);
+    }, []);
+    return pokemons;
+}
+
+export default usePokemons;
--- src/javascript/pages/pokemon-list.tsx
+++ src/javascript/pages/pokemon-list.tsx
@@ -2,13 +2,10 @@ import React, {FunctionComponent, useState, useEffect} from 'react';
 import Pokemon from '../models/pokemon';
 import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
+import usePokemons from "../hooks/pokemon.hook";
 
 const PokemonList: FunctionComponent = () => {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-    useEffect(() => {
-        setPokemons(POKEMONS);
-    }, []);
+    const pokemons = usePokemons();
 
     return (<div style={{overflowY: 'scroll'}}>
         <h1 className="center">Pokédex</h1>
