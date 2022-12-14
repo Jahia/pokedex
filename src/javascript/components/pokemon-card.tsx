f18768968553250463b6bc6a7bd597a0cdaedfe9
Florent BOURASSE <fbourasse@jahia.com>
2022-12-14 15:12:57 +0100

    Usage of custom theme to prevent conflict between Jahia and Materliaze CSS
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/components/pokemon-card.tsx
+++ src/javascript/components/pokemon-card.tsx
@@ -1,7 +1,7 @@
 import React, {FunctionComponent, useEffect, useState} from 'react';
 import Pokemon from '../models/pokemon';
-import '../materialize.min.css';
 import './pokemon-card.css';
+import '../theme.css';
 
 type Props = {
     pokemon: Pokemon,
